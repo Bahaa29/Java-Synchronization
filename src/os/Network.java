8b1cef7bd9f9bece5f2af8db85671df0c5945fbc
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2020-12-04 17:27:05 +0200

    Network Class Finished & Start of Router Class

--- src/os/Network.java
+++ src/os/Network.java
@@ -1,4 +1,29 @@
 package os;
 
+import java.util.Vector;
+
 public class Network {
+    private int ConnectionNo;
+    private int WaitingListLimit;
+    private Vector<String> Devices_List;
+    //Getters
+    public int getConnectionNo() {
+        return ConnectionNo;
+    }
+    public void setConnectionNo(int connectionNo) {
+        ConnectionNo = connectionNo;
+    }
+    public int getWaitingListLimit() {
+        return WaitingListLimit;
+    }
+    //Setters
+    public void setWaitingListLimit(int waitingListLimit) {
+        WaitingListLimit = waitingListLimit;
+    }
+    public Vector<String> getDevices_List() {
+        return Devices_List;
+    }
+    public void setDevices_List(Vector<String> devices_List) {
+        Devices_List = devices_List;
+    }
 }
--- src/os/Router.java
+++ src/os/Router.java
@@ -1,4 +1,19 @@
 package os;
 
 public class Router {
+    private int ConnectionsNo;
+    private Boolean[] ConnectionsArr;
+    private Semaphore semaphore;
+
+    Router(int x){
+        ConnectionsNo = x;
+        ConnectionsArr = new Boolean[x];
+        semaphore = new Semaphore(x);
+    }
+    public synchronized int Occupy(Device device){
+        for(int i=0;i<ConnectionsNo;i++){
+            if(ConnectionsArr[i] == false)
+                break;
+        }
+    }
 }
