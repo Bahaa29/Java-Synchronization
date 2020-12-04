d18f2e75e19718fab7723ab2abb5c2bb4ca0e2bf
Bahaa <bahaaeldin390@gmail.com>
2020-12-04 06:02:20 -0800

    semphore class

--- src/os/Device.java
+++ src/os/Device.java
@@ -1,4 +1,11 @@
 package os;
 
-public class Device {
+public class Device extends Thread
+{
+    public Router object;
+    Device(String clientName, Router router)
+    {
+        super(clientName);
+        object = router;
+    }
 }
--- src/os/Semaphore.java
+++ src/os/Semaphore.java
@@ -1,4 +1,34 @@
 package os;
 
-public class Semaphore {
+public class Semaphore
+{
+    public static int WIFI_Connection;
+    //constructors
+    Semaphore(int number)
+    {
+        WIFI_Connection=number;
+    }
+    public synchronized void P(Device device)
+    {
+        WIFI_Connection--;
+        if(WIFI_Connection<0)
+        {
+            try {
+                System.out.println(device.getName() + " arrived and waiting");
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            System.out.println(device.getName() + " arrived");
+        }
+    }
+    public synchronized void V() {
+        WIFI_Connection++;
+        if (WIFI_Connection <= 0)
+            notify();
+    }
 }
+
